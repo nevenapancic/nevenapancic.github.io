b552b94dd64a867a64af102e35b40583e89a1231
nevenapancic <nevenapancic97@gmail.com>
2025-12-11 19:37:13 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className='bg-[#F0E491] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#BBC863] opacity-70'></div>
-        <div className='bg-[#658C58] absolute top-[-1rem] -z-10 left-[-5rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#31694E] opacity-70'></div>
+        <div className='bg-[#658C58] absolute top-[-1rem] -z-10 left-0 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#31694E] opacity-80'></div>
 
         <ThemeContextProvider>
           <LanguageContextProvider>
