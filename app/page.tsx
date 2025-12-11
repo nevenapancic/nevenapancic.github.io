66fa01ee664b2a83349c8d1e338cf4128decd0ca
nevenapancic <nevenapancic97@gmail.com>
2025-12-11 19:20:49 +0100

    fix building

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import About from '@/components/about';
 import Contact from '@/components/contact';
 import Experience from '@/components/experience';
 import Intro from '@/components/intro';
-import Projects from '@/components/projects';
+// import Projects from '@/components/projects';
 import SectionDivider from '@/components/sectionDivider';
 import Skills from '@/components/skills';
 import Education from '@/components/education';
