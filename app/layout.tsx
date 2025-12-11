bf63bb65bc206d01e2c0422f7bae4e6023449196
nevenapancic <nevenapancic97@gmail.com>
2025-12-11 13:56:54 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,28 @@
-import Header from "@/components/header";
-import "./globals.css";
-import { Inter } from "next/font/google";
-import ActiveSectionContextProvider from "@/context/activeSectionContext";
-import Footer from "@/components/footer";
-import ThemeSwitch from "@/components/themeSwitch";
-import ThemeContextProvider from "@/context/themeContext";
-import { Toaster } from "react-hot-toast";
+/** @format */
 
-const inter = Inter({ subsets: ["latin"] });
+import Header from '@/components/header';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import ActiveSectionContextProvider from '@/context/activeSectionContext';
+import Footer from '@/components/footer';
+import ThemeSwitch from '@/components/themeSwitch';
+import ThemeContextProvider from '@/context/themeContext';
+import { Toaster } from 'react-hot-toast';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "Nevena | Personal Portfolio",
-  description: "Nevena is a Junior Full-Stack Developer with a specialization in Front-end.",
+  title: 'Nevena | Personal Portfolio',
+  description:
+    'Nevena is a Full-Stack Developer specializing in React, TypeScript, and cloud technologies.',
+  icons: {
+    icon: [
+      {
+        url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👩‍💻</text></svg>",
+        type: 'image/svg+xml',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -20,12 +31,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="!scroll-smooth">
+    <html lang='en' className='!scroll-smooth'>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]'></div>
+        <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]'></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
@@ -33,7 +44,7 @@ export default function RootLayout({
             {children}
             <Footer />
 
-            <Toaster position="top-right" />
+            <Toaster position='top-right' />
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
--- components/about.tsx
+++ components/about.tsx
@@ -1,49 +1,63 @@
-"use client";
+/** @format */
 
-import React from "react";
-import SectionHeading from "./sectionHeading";
-import { motion } from "framer-motion";
-import { useSectionInView } from "@/lib/hooks";
+'use client';
+
+import React from 'react';
+import SectionHeading from './sectionHeading';
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function About() {
-  const { ref } = useSectionInView("About");
+  const { ref } = useSectionInView('About');
 
   return (
     <motion.section
       ref={ref}
-      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      id="about"
+      id='about'
     >
       <SectionHeading>About me</SectionHeading>
-      <p className="mb-3">
-        After completing a bachelor's degree in{" "}
-        <span className="font-medium">Biology</span> and two master's degrees—one in{" "}
-        <span className="font-medium">Environmental Policy</span> and another in{" "}
-        <span className="font-medium">Environment and Resource Management</span>—I decided to pursue my
-        passion for programming. I enrolled in the{" "}
-        <span className="font-medium">Le Wagon</span> coding bootcamp, where I learned{" "}
-        <span className="font-medium">full-stack web development</span>.
+      <p className='mb-3'>
+        After completing a bachelor's degree in{' '}
+        <span className='font-medium'>Biology</span> and two master's
+        degrees—one in <span className='font-medium'>Environmental Policy</span>{' '}
+        and another in{' '}
+        <span className='font-medium'>Environment and Resource Management</span>
+        —I decided to pursue my passion for programming. I enrolled in the{' '}
+        <span className='font-medium'>Le Wagon</span> coding bootcamp, where I
+        learned <span className='font-medium'>full-stack web development</span>.
       </p>
 
-      <p className="mb-3">
-        What I enjoy most about programming is the problem-solving aspect; I{" "}
-        <span className="italic">love the moment when everything finally clicks</span> and a solution comes together. My core stack includes{" "}
-        <span className="font-medium">
+      <p className='mb-3'>
+        What I enjoy most about programming is the problem-solving aspect; I{' '}
+        <span className='italic'>
+          love the moment when everything finally clicks
+        </span>{' '}
+        and a solution comes together. My core stack includes{' '}
+        <span className='font-medium'>
           React, Next.js, TypeScript, and Ruby on Rails
         </span>
-        . I'm always eager to explore new technologies and continuously expand my skill set.
+        . I'm always eager to explore new technologies and continuously expand
+        my skill set.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy running halfmarathones (I am a proud owner of 25+ medals) and sweing clothes. In fact I had my own fashion brand Kalas <a href="https://www.instagram.com/__kalas___/?hl=en" target="_blank" rel="noopener noreferrer">
-        <span className="underline">(check it out)</span>.
-</a>  I also enjoy{" "}
-        <span className="font-medium">learning new things</span>. I am currently
-        learning more about{" "}
-        <span className="font-medium">Project management</span>.
+        <span className='italic'>When I'm not coding</span>, I enjoy running
+        halfmarathones (I am a proud owner of 25+ medals) and sweing clothes. In
+        fact I had my own fashion brand Kalas{' '}
+        <a
+          href='https://www.instagram.com/__kalas___/?hl=en'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <span className='underline'>(check it out)</span>.
+        </a>{' '}
+        I also enjoy <span className='font-medium'>learning new things</span>. I
+        am currently learning more about{' '}
+        <span className='font-medium'>Project management</span>.
       </p>
     </motion.section>
   );
